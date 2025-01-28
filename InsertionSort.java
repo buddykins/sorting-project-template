d3dce9ea169a348b43b1d0bd802f4ae59d43371e
buddykins <throuse6@gmail.com>
2025-01-28 03:20:20 +0000

    Pending changes exported from your codespace

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,32 @@
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+        int[] stored = {0,input.length};
+        int storage;
+        for (int i = 0; i < input.length; i++){
+            storage = input[i];
+            if (i == 0){
+                stored[i] = input[i];
+            }
+            else{
+                System.out.println(stored); // comment out once it works
+                for (int l = 0; l < stored.length; l++){
+
+                }
+              
+            }
+        }
+    }
+    public int[] insertBefore(int index, int number, int[] stored){
+        int[] inserted = {0,stored.length};
+        for (int j = index; j < stored.length; j++){
+            if (j == index){
+                inserted[j] = number;
+            }
+            else{
+                inserted[j] = stored[j--]; 
+            }
+        }
+        return inserted;
     }
 }
